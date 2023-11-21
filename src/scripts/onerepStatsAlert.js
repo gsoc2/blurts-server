997515924c933da2177e9927eb110eb3caffed4f
Robert Helmer <rhelmer@mozilla.com>
2023-11-20 20:37:31 -0800

    Update src/scripts/onerepStatsAlert.js
    Co-authored-by: Peter deHaan <pdehaan@hey.com>

--- src/scripts/onerepStatsAlert.js
+++ src/scripts/onerepStatsAlert.js
@@ -56,7 +56,7 @@ async function onerepFetch(path, options) {
   }
   const onerepApiKey = process.env.ONEREP_API_KEY;
   if (!onerepApiKey) {
-    throw new Error("ONEREP_API_BASE env var not set");
+    throw new Error("ONEREP_API_KEY env var not set");
   }
   const url = new URL(path, onerepApiBase);
   const headers = new Headers(options?.headers);
